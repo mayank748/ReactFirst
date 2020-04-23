fb01ed65d879c3f9fc98df9e6d7bb51df246d697
Mayank <smayank748@gmail.com>
2020-04-23 20:40:46 +0530

    Updates

---
+++ precache-manifest.51eef4c14464fe3d9851459f88c83a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831e020e9e28084df7c187d5c3f8c018",
+    "url": "/ReactFirst/index.html"
+  },
+  {
+    "revision": "acbe6f7793bd6e184a92",
+    "url": "/ReactFirst/static/css/main.fd9b09b9.chunk.css"
+  },
+  {
+    "revision": "ae963fcebde0aa1f0488",
+    "url": "/ReactFirst/static/js/2.b59722a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFirst/static/js/2.b59722a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbe6f7793bd6e184a92",
+    "url": "/ReactFirst/static/js/main.09d56613.chunk.js"
+  },
+  {
+    "revision": "4c880b7be39a733cfd02",
+    "url": "/ReactFirst/static/js/runtime-main.4d93f921.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactFirst/static/media/logo.5d5d9eef.svg"
+  }
+]);
